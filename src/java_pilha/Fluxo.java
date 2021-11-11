0d00fe7d335ec0add14b974d5cd7dc51cad3e3dc
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-11 15:25:18 -0300

    Learnig to use Multi Catch

--- src/java_pilha/Fluxo.java
+++ src/java_pilha/Fluxo.java
@@ -6,8 +6,10 @@ public class Fluxo {
         System.out.println("Ini do main");
         try {
         metodo1();
-    	} catch (ArithmeticException ex) {
-    		System.out.println("ArithmeticException");
+    	} catch (ArithmeticException | NullPointerException ex) { // ex eh uma referencia e ArithmeticException eh uma classe
+    		String msg = ex.getMessage();
+    		System.out.println("Exception" + msg);
+    		ex.printStackTrace();
     	}
         System.out.println("Fim do main");
     }
@@ -22,7 +24,9 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
-            int a = i / 0;
+            // int a = i / 0;
+            Conta c = null;
+            c.deposita();
         System.out.println("Fim do metodo2");
         }
     }
